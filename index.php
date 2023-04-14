595b92e393ec4dfa4faaf24fe57e85e33ecb8965
unknown <joshembling@gmail.com>
2023-04-14 23:10:06 +0100

    Undo private

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $array = [1,2,3,4,5,6,7,8,9,10];
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Deployment</title>
   </head>
   <body>
     <h1>Deployment test</h1>
